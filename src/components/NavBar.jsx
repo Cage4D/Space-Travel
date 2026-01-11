ee12e12ec3fe4846cc2b64184bf02ef4a96a329e
Johnny Cage <thespammingbott@gmail.com>
2026-01-11 18:37:06 +0100

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+function NavBar() {
+  return (
+    <nav class="nav_bar">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="logo"
+        width="48"
+        height="48"
+      >
+        <g fill="none" fill-rule="evenodd">
+          <circle cx="24" cy="24" r="24" fill="#FFF" />
+          <path
+            fill="#0B0D17"
+            d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+          />
+        </g>
+      </svg>
+      <p class="nav-line"></p>
+      <div class="nav-container">
+        <ul class="nav-container__list">
+          <li>
+            <span>00</span>Home
+          </li>
+          <li>
+            <span>01</span>Destination
+          </li>
+          <li>
+            <span>02</span>Crew
+          </li>
+          <li>
+            <span>03</span>Technology
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
