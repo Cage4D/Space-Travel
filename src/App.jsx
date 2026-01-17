bb7d3485d4ee8acd1c6aa053ba768a88609baea0
Johnny Cage <thespammingbott@gmail.com>
2026-01-17 22:16:43 +0100

    update: change class to className

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ function App() {
   return (
     <>
       <NavBar/>
-      <section class="home">
-        <p class="home-text">
-          <span class="home-text__travel">so, you want to travel to</span>
-          <span class="home-text__space">space</span>
+      <section className="home">
+        <p className="home-text">
+          <span className="home-text__travel">so, you want to travel to</span>
+          <span className="home-text__space">space</span>
           Let's face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we'll give you a truly out of this world
           experience!
         </p>
-        <a href="#" class="home-explore">
+        <a href="#" className="home-explore">
           Explore
         </a>
       </section>
