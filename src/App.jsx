3e913e934218b83a841ada913f3b2ca916c0efa1
Johnny Cage <thespammingbott@gmail.com>
2026-01-17 22:22:32 +0100

    fix: missing anchor closing ta

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           back, and relax because we'll give you a truly out of this world
           experience!
         </p>
-        <a href="#" className="home-explore">
+        <a href="#" className="home-explore"></a>
         <a href="#" className="home-explore">
           Explore
         </a>
