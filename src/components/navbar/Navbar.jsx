bcff86d939f072c357050c522bc9775fdd4127cc
Johnny Cage <thespammingbott@gmail.com>
2026-01-20 18:22:06 +0100

    update: replace svg static width and height properties with viewbox alongside getting nested  in .logo-container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,20 +3,21 @@ import "./navbar.css";
 function Navbar() {
   return (
     <nav className="nav_bar">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="logo"
-        width="48"
-        height="48"
-      >
-        <g fill="none" fillRule="evenodd">
-          <circle cx="24" cy="24" r="24" fill="#FFF" />
-          <path
-            fill="#0B0D17"
-            d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-          />
-        </g>
-      </svg>
+      <div className="logo-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="logo"
+          viewBox="0 0 48 48"
+        >
+          <g fill="none" fillRule="evenodd">
+            <circle cx="24" cy="24" r="24" fill="#FFF" />
+            <path
+              fill="#0B0D17"
+              d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
+            />
+          </g>
+        </svg>
+      </div>
       <p className="nav-line"></p>
       <input type="checkbox" className="menu-toggle" id="menu-toggle" />
       <label htmlFor="menu-toggle" className="hamburger">
