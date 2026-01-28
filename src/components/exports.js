d8a3e7e0743eb54e1e917279463f0843a4f06c11
Johnny Cage <thespammingbott@gmail.com>
2026-01-28 07:59:25 +0100

    add Crew and Technology default exports

--- src/components/exports.js
+++ src/components/exports.js
@@ -1,5 +1,5 @@
-export { default as Navbar } from "./navbar/Navbar"
-export { default as Home } from "./home/Home"
-export { default as Destination } from "./Destination/Destination"
-export { default as Crew } from "./Crew/Crew"
-export {default as Technology } from "./Technology/Technology"
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Home } from "./home/Home";
+export { default as Destination } from "./Destination/Destination";
+export { default as Crew } from "./Crew/Crew";
+export { default as Technology } from "./Technology/Technology";
