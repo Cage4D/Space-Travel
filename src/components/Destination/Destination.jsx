e730198192de874cc61f92b721bc13461883a7d7
Johnny Cage <thespammingbott@gmail.com>
2026-02-01 17:07:40 +0100

    implement layout skeleton

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -2,7 +2,17 @@ import "./destination.css"
 
 function Destination() {
     return (
-        <div className="destination-container"></div>
+        <div className="destination-container">
+            <section className="destination-contents">
+                <div className="destination-contents__image-container">
+                    <p className="destination-text"><span>01</span>pick your destination</p>
+                    <div className="img-container">
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div className="destination-contents__info"></div>
+            </section>
+        </div>
     )
 }
 
