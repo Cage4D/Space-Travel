983a5ce9e5f676bd129780329a445deb5c02df60
Johnny Cage <thespammingbott@gmail.com>
2026-02-08 07:39:56 +0100

    chore(assets): add destination image exports

---
+++ src/components/Destination/exports.js
@@ -0,0 +1,8 @@
+export { default as moonPng } from "../../assets/destination/image-moon.png"
+export { default as moonWebp } from "../../assets/destination/image-moon.webp"
+
+export { default as europaPng } from "../../assets/destination/image-europa.png"
+export { default as europaWebp } from "../../assets/destination/image-europa.webp"
+
+export { default as marsPng } from "../../assets/destination/image-mars.png"
+export { default as marsWebp } from "../../assets/destination/image-mars.webp"
