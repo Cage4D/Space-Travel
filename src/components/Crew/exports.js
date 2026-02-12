209cd9016f7b009b7f6fd24205d6fa829e2de8e4
Johnny Cage <thespammingbott@gmail.com>
2026-02-12 15:44:46 +0100

    import projects files and assets

---
+++ src/components/Crew/exports.js
@@ -0,0 +1,11 @@
+import douglasHurleyWebp from "../../assets/crew/image-douglas-hurley.webp"
+import douglasHurleyPng from "../../assets/crew/image-douglas-hurley.png"
+
+import shuttleworthWebp from "../../assets/crew/image-mark-shuttleworth.webp"
+import shuttlewortPng from "../../assets/crew/image-mark-shuttleworth.png"
+
+import victorGloverWebp from "../../assets/crew/image-victor-glover.webp"
+import victorGloverPng from "../../assets/crew/image-victor-glover.png"
+
+import anousheshAnsariWebp from "../../assets/crew/image-anousheh-ansari.webp"
+import anousheshAnsariPng from "../../assets/crew/image-anousheh-ansari.png"
