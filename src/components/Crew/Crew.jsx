2917b0ebc85afb1db93982480eb8834b6f3c286c
Johnny Cage <thespammingbott@gmail.com>
2026-02-12 17:46:38 +0100

    feat: implement Crew component with dynamic member switching

--- src/components/Crew/Crew.jsx
+++ src/components/Crew/Crew.jsx
@@ -1,28 +1,32 @@
+import React from "react"
 import "./crew.css"
-import douglasHurleyWebp from "../../assets/crew/image-douglas-hurley.webp"
-import douglasHurleyPng from "../../assets/crew/image-douglas-hurley.png"
+import { contents } from "./exports"
 
 function Crew() {
+    const [member, setMember] = React.useState(contents[0])
     return <div className="crew-container">
         <section className="crew-contents">
             <div className="crew-contents__info">
                 <p className="crew-text"><span>02</span>Meet your crew</p>
-                <h4 className="crew-member-position">Mission Specialist</h4>
-                <h3 className="crew-member-name">Mark Shuttleworth</h3>
-                <p className="crew-member-about">Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.</p>
+                <h4 className="crew-member-position">{member.position}</h4>
+                <h3 className="crew-member-name">{member.name}</h3>
+                <p className="crew-member-about">{member.about}</p>
                 <div className="crew-button-group">
-                    <button type="button"></button>
-                    <button type="button"></button>
-                    <button type="button"></button>
-                    <button type="button"></button>
+                    {contents.map(item => (
+                        <button 
+                        key={item.name} 
+                        type="button" 
+                        className={`${member.name === item.name ? "crew-active crew-btn" : "crew-btn"}`}
+                        onClick={() => setMember(item)}></button>
+                    ))}
                 </div>
             </div>
             <div className="crew-img-container">
                 <picture>
-                    <source srcSet={douglasHurleyWebp} type="image/webp"/>
+                    <source srcSet={member.webp} type="image/webp"/>
                     <img 
-                    src={douglasHurleyPng} 
-                    alt="douglas hurley"/>
+                    src={member.png} 
+                    alt={member.name}/>
                 </picture>
             </div>
         </section>
