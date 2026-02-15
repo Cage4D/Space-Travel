8cc7db510e15c62a40002b47f02b75e700f5ba48
Johnny Cage <thespammingbott@gmail.com>
2026-02-15 06:47:53 +0100

    add image imports

---
+++ src/components/Technology/exports.js
@@ -0,0 +1,8 @@
+import launchVehicleLandScape from "../../assets/techonology/image-launch-vehicle-landscape.jpg"
+import launchVehiclePortrait from "../../assets/techonology/image-launch-vehicle-portrait.jpg"
+
+import spaceCapsuleLandscape from "../../assets/technology/image-space-capsule-landscape.jpg"
+import spaceCapsulePortrait from "../../assets/technology/image-space-capsule-portrait.jpg"
+
+import spaceportLandscape from "../../assets/technology/image-spaceport-landscape.jpg"
+import spaceportPortrait from "../../assets/technology/image-spaceport-portrait.jpg"
