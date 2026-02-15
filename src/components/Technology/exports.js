4a776e3d5949e83c5fcc75761349466ecae27d46
Johnny Cage <thespammingbott@gmail.com>
2026-02-15 07:08:51 +0100

    fix technology type in the first two lines & add contents export

--- src/components/Technology/exports.js
+++ src/components/Technology/exports.js
@@ -1,8 +1,29 @@
-import launchVehicleLandScape from "../../assets/techonology/image-launch-vehicle-landscape.jpg"
-import launchVehiclePortrait from "../../assets/techonology/image-launch-vehicle-portrait.jpg"
+import launchVehicleLandScape from "../../assets/technology/image-launch-vehicle-landscape.jpg"
+import launchVehiclePortrait from "../../assets/technology/image-launch-vehicle-portrait.jpg"
 
 import spaceCapsuleLandscape from "../../assets/technology/image-space-capsule-landscape.jpg"
 import spaceCapsulePortrait from "../../assets/technology/image-space-capsule-portrait.jpg"
 
 import spaceportLandscape from "../../assets/technology/image-spaceport-landscape.jpg"
 import spaceportPortrait from "../../assets/technology/image-spaceport-portrait.jpg"
+
+export const contents = [
+    {
+        header: "LAUNCH VEHICLE",
+        about: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+        landscapeImg: launchVehicleLandScape,
+        portraitImg: launchVehiclePortrait,
+    },
+    {
+        header: "SPACEPORT",
+        about: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+        landscapeImg: spaceportLandscape,
+        portraitImg: spaceportPortrait,
+    },
+    {
+        header: "SPACE CAPSULE",
+        about: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+        landscapeImg: spaceCapsuleLandscape,
+        portraitImg: spaceCapsulePortrait,
+    },
+]
